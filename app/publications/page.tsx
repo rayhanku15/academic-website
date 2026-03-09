6d5882338475ac07a539554525671fd4a13653be
Md Sayeed Abu Rayhan <mrayhan@sycamores.indstate.edu>
2026-03-09 15:59:15 -0400

    fix publications page

--- app/publications/page.tsx
+++ app/publications/page.tsx
@@ -4,28 +4,28 @@ export default function Publications() {
 
       <h1 className="text-3xl font-semibold mb-6">Publications</h1>
 
-      <ul className="space-y-4">
+      <ul className="list-disc pl-6 space-y-4">
 
         <li>
-          <strong>
-          Farmer AJ, Katariya R, Islam S, Rayhan MSA, Inlow MH, Ahmad SM, Schwab KR. trithorax is an 
-essential regulator of cardiac Hox gene expression and anterior-posterior patterning of the 
-Drosophila embryonic heart tube. Biol Open. 2025 Apr 15;14(4): bio061919. doi: 
+          Farmer AJ, Katariya R, Islam S, Rayhan MSA, Inlow MH, Ahmad SM, Schwab KR.
+          trithorax is an essential regulator of cardiac Hox gene expression and
+          anterior-posterior patterning of the Drosophila embryonic heart tube.
+          Biol Open. 2025 Apr 15;14(4): bio061919. doi: 10.1242/bio.061919.
         </li>
 
         <li>
-          Abu Rayhan MS, Rahman MS, Bose PK, Sarower MG, Ali MY. Estimating peak breeding season, 
-size at first maturity and variation in fecundity and egg-size at different sizes of Hilsa (Tenualosa 
-ilisha). Heliyon. 2023 Aug 23;9(9): e19420. doi: 10.1016/j.heliyon. 2023.e19420. 
-          
-        </li>
-        <li>
-          Ali, M.Y., Rayhan, M.S.A., Rahman, M.B. (2024) Identification and comparative analysis of Heat 
-Shock Protein genes Hsp70 and Hsp90 from gill transcriptomes of three Australian crayfish. 
-Egyptian journal of Aquatic Biology and Fisheries (In revision).  
-          
+          Abu Rayhan MS, Rahman MS, Bose PK, Sarower MG, Ali MY.
+          Estimating peak breeding season, size at first maturity and variation in
+          fecundity and egg-size at different sizes of Hilsa (Tenualosa ilisha).
+          Heliyon. 2023 Aug 23;9(9):e19420.
         </li>
 
+        <li>
+          Ali MY, Rayhan MSA, Rahman MB. (2024)
+          Identification and comparative analysis of Heat Shock Protein genes
+          Hsp70 and Hsp90 from gill transcriptomes of three Australian crayfish.
+          Egyptian Journal of Aquatic Biology and Fisheries (In revision).
+        </li>
 
       </ul>
 
