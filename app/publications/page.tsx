50003559c16697fb85587a9409b30edd99e2ad36
Md Sayeed Abu Rayhan <mrayhan@sycamores.indstate.edu>
2026-03-09 15:00:21 -0400

    update website

--- app/publications/page.tsx
+++ app/publications/page.tsx
@@ -7,16 +7,25 @@ export default function Publications() {
       <ul className="space-y-4">
 
         <li>
-          <strong>Rayhan, M. S. A.</strong> (2024).
-          Epigenetic regulation of Hox genes during cardiac development.
-          <em> Journal of Developmental Biology.</em>
+          <strong>
+          Farmer AJ, Katariya R, Islam S, Rayhan MSA, Inlow MH, Ahmad SM, Schwab KR. trithorax is an 
+essential regulator of cardiac Hox gene expression and anterior-posterior patterning of the 
+Drosophila embryonic heart tube. Biol Open. 2025 Apr 15;14(4): bio061919. doi: 
         </li>
 
         <li>
-          Rayhan, M. S. A., et al. (2023).
-          Molecular mechanisms of cardiac patterning.
-          <em> Developmental Genetics.</em>
+          Abu Rayhan MS, Rahman MS, Bose PK, Sarower MG, Ali MY. Estimating peak breeding season, 
+size at first maturity and variation in fecundity and egg-size at different sizes of Hilsa (Tenualosa 
+ilisha). Heliyon. 2023 Aug 23;9(9): e19420. doi: 10.1016/j.heliyon. 2023.e19420. 
+          
         </li>
+        <li>
+          Ali, M.Y., Rayhan, M.S.A., Rahman, M.B. (2024) Identification and comparative analysis of Heat 
+Shock Protein genes Hsp70 and Hsp90 from gill transcriptomes of three Australian crayfish. 
+Egyptian journal of Aquatic Biology and Fisheries (In revision).  
+          
+        </li>
+
 
       </ul>
 
